6b79f60f3832156c38be174917c8b85e1808ec56
artemzdor <artemzdor@gmail.com>
2018-05-08 22:55:59 +0300

    +

---
+++ MasterDesktop/MasterDesktop/Lib/Forms/FormSettings.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MasterDesktop.Lib.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для FormSettings.xaml
+    /// </summary>
+    public partial class FormSettings : Window
+    {
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            //Application.Current.Shutdown();
+        }
+
+
+        public FormSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MasterDesktop/MasterDesktop/MainWindow.xaml.cs
+++ MasterDesktop/MasterDesktop/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MasterDesktop.Lib;
 using MasterDesktop.Lib.Data;
+using MasterDesktop.Lib.Forms;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Newtonsoft.Json;
+
 
 namespace MasterDesktop
 {
@@ -119,22 +121,20 @@ namespace MasterDesktop
 
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void Exit_Executed(object sender, ExecutedRoutedEventArgs e) => Close();
 
-
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FormSettings fs = new FormSettings();
+            fs.Owner = this;
+            //this.Hide();
+            fs.ShowDialog();
+        }
     }
 
     public class WindowCommands
     {
-        static WindowCommands()
-        {
-            Exit = new RoutedCommand("Exit", typeof(MainWindow));
-        }
-        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand Exit { get; set; } = new RoutedCommand("Exit", typeof(MainWindow));
+        
     }
 }

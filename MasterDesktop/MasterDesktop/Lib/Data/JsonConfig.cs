9ceea79656f0cdb23e3733f153637b1bceb7ac99
artemzdor <artemzdor@gmail.com>
2018-05-08 23:49:00 +0300

    +

---
+++ MasterDesktop/MasterDesktop/Lib/Data/JsonConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDesktop.Lib.Data
+{
+    public class JsonConfig
+    {
+        public string HostServis { get; set; }
+        public string PortServis { get; set; }
+        public string DB_Charset { get; set; }
+        public string DB_User { get; set; }
+        public string DB_Password { get; set; }
+        public string DB_Connect { get; set; }
+    }
+}
